d4911f689b21ba5994b4d77b978971cf8e678871
Zeous_Shrey <shrey.sadhukhan21@gmail.com>
2023-08-28 14:47:54 +0530

    shrey

--- components/server/server-search.tsx
+++ components/server/server-search.tsx
@@ -71,7 +71,10 @@ export const ServerSearch = ({
         <kbd
           className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground ml-auto"
         >
-          <span className="text-xs">⌘</span>K
+          <span className="text-xs">
+            {process.platform === "darwin" ? "⌘" : "Ctrl"}
+            
+          </span>K
         </kbd>
       </button>
       <CommandDialog open={open} onOpenChange={setOpen}>
